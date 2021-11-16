eb9b91b9ec47da52674e745be27b7cce4900f1b7
Meerwaldten <mikkell_m_j@hotmail.com>
2021-11-16 14:37:20 +0100

    Final (?) commit

--- src/HaikuApp.java
+++ src/HaikuApp.java
@@ -12,10 +12,9 @@ public class HaikuApp {
      HaikuPoem testPoem = new HaikuPoem("Mikkel", "sitting on a rock","i watch the tall grass blow slow", "and birds faintly sing");
      HaikuPoem testPoem1 = new HaikuPoem("Mads","swirling and twirling","sloshing, splasing and crashing","all wrinkling with fish");
      app.menu.welcomeToApp();
-     //app.menu.typeInHaikuPoem();
-     //System.out.println(testPoem);
-     //app.checker.haikuChecker(testPoem);
-     app.checker.haikuChecker(testPoem1);
+     //app.menu.typeInHaikuPoem(); // Method that'll allow the user to type in their own poem, and it'll then get checked and verified.
+     app.checker.haikuChecker(testPoem); // Testers
+     app.checker.haikuChecker(testPoem1); // Testers
     }
 
 }
--- src/data/Uploader.java
+++ src/data/Uploader.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 
 public class Uploader {
 
+    /*
+    Couldn't figure out how to make it save the file, so at this point it'll keep overwriting the old file instead of adding more poems to it.
+     */
+
     public void uploadToFile(String author, int id, String firstLine, String secondLine, String thirdLine) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("src/data/uploadedPoems"));
